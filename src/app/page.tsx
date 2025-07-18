e8935bfca0385418d880600ad54b1b5f6b320b04
dipu67 <mddipu2482@gmail.com>
2025-07-18 15:49:43 +0600

    deleted:    a24_image-1752819184224.png 	deleted:    a24_image-1752819273667.png 	deleted:    a24_image-1752819426567.png 	deleted:    a24_image-1752819513518.png 	deleted:    a24_image-1752820212726.png 	deleted:    a24_image-1752820919657.png 	deleted:    a24_image-1752820988012.png 	deleted:    a24_image-1752821075506.png 	deleted:    a24_img1752819089777.png 	deleted:    gemini-native-image.png 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,35 @@ import bot from "@/lib/tgbot/bot";
 import { aiClient } from "@/lib/gemini/ai";
 import { Modality } from "@google/genai";
 import fs from "fs";
+import { Readable } from "stream";
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
 
   if (msg.text && msg.text.startsWith("/image")) {
     const mg = await bot.sendMessage(chatId, "Generate Image...");
+    
     const res = await image(msg.text);
+    if (!res || !res.buffer || res.buffer.length === 0) {
+      bot.sendMessage(chatId, "Sorry, I could not process your request.");
+      return;
+    }
+    const [buffer] = res.buffer; // Assuming the first buffer is the image
+    const [text] = res.text; // Assuming the first text is the caption
+
+
+    bot.deleteMessage(chatId, mg.message_id);
+    bot.sendPhoto(
+      chatId,
+      buffer,
+      {
+        caption: text || "Here is your image:",
+      },
+      { contentType: "image/png", filename: `a24_image-${Date.now()}.png` }
+    );
+  } else if (msg.text && msg.text.startsWith("/video")) {
+    const mg = await bot.sendMessage(chatId, "Generate Video...");
+    const res = await video(msg.text);
 
     if (!res!) {
       bot.sendMessage(chatId, "Sorry, I could not process your request.");
@@ -18,15 +40,13 @@ bot.on("message", async (msg) => {
     }
 
     bot.deleteMessage(chatId, mg.message_id);
-    bot.sendPhoto(
+    bot.sendVideo(
       chatId,
       res,
       {
-        caption: "Here is your image:",
+        caption: "Here is your video:",
       },
-      { contentType: "image/png" ,
-        filename: res
-      }
+      { contentType: "video/mp4", filename: res }
     );
   } else {
     const mg = await bot.sendMessage(chatId, "Writing...");
@@ -53,7 +73,7 @@ async function resAi(data: any) {
   }
 }
 
-async function image(data: any) {
+async function image(data: any): Promise<any> {
   try {
     // Set responseModalities to include "Image" so the model can generate  an image
     const response = await aiClient.models.generateContent({
@@ -67,22 +87,55 @@ async function image(data: any) {
       throw new Error("No candidates returned from AI model.");
     }
     const parts = response.candidates?.[0]?.content?.parts ?? [];
+    const texts: string[] = [];
+    const buffers: Buffer[] = [];
+
     for (const part of parts) {
       // Based on the part type, either show the text or save the image
       if (part.text) {
-        console.log(part.text);
+        texts.push(part.text);
       } else if (part.inlineData) {
         const imageData = part.inlineData.data;
         const buffer = Buffer.from(imageData!, "base64");
-        const fileName = `a24_image-${Date.now()}.png`;
-        fs.writeFileSync(fileName, buffer);
-
-        return fileName; // Return the file name or path for further processing
+        buffers.push(buffer);
+        // Return the file name or path for further processing
       }
     }
+    return { text: texts, buffer: buffers }; // Return the text and image buffer
   } catch (error) {
     console.error("Error fetching AI image:", error);
   }
+}
+
+async function video(data: any) {
+  let operation = await aiClient.models.generateVideos({
+    model: "veo-3.0-generate-preview",
+    prompt: data,
+    config: {
+      personGeneration: "allow_all",
+      aspectRatio: "16:9",
+    },
+  });
+
+  while (!operation.done) {
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    operation = await aiClient.operations.getVideosOperation({
+      operation: operation,
+    });
+  }
+
+  operation.response?.generatedVideos?.forEach(async (generatedVideo, n) => {
+    const resp = await fetch(
+      `${generatedVideo.video?.uri}${process.env.GOOGLE_GENAI_API_KEY}`
+    ); // append your API key
+    const writer = fs.createWriteStream(`video${n}.mp4`);
+    Readable.fromWeb(resp.body).pipe(writer);
+    writer.on("finish", () => {
+      console.log(`Video ${n} downloaded successfully.`);
+    });
+
+    return `video${n}.mp4`; // Return the file name or path for further processing
+  });
 }
 
 console.log("Telegram bot is running...");
